575adbe5c90f044a5a712c90fb5a79e77dbaf06f
Fanline <v.valera2007@gmail.com>
2026-05-23 16:26:16 +0300

    add validation for server endpoint

--- src/PulsePanel.Core/DTOs/Servers/CreateServerRequest.cs
+++ src/PulsePanel.Core/DTOs/Servers/CreateServerRequest.cs
@@ -1,13 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace PulsePanel.Core.DTOs.Servers
 {
     public class CreateServerRequest
     {
+        [Required]
+        [MinLength(2)]
+        [MaxLength(100)]
         public required string Name { get; set; }
+        [Required]
+        [MaxLength(255)]
         public required string Host { get; set; }
+        [MaxLength(500)]
         public string? Description { get; set; }
     }
 }
